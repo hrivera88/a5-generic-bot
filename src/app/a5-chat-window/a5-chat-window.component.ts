51b2b9c565d3de16bc766b26bbb3c64e35f35b64
alivehal <hrivera@outlook.com>
2019-02-12 14:20:52 -0600

    Removed stuff

--- src/app/a5-chat-window/a5-chat-window.component.ts
+++ src/app/a5-chat-window/a5-chat-window.component.ts
@@ -359,11 +359,13 @@ export class A5ChatWindowComponent implements OnInit {
 
   chooseBotOption(evt: any) {
     let optionText = evt.target.value;
-    if (optionText === "buy movie") {
-      this.showAlivePayModal = true;
-      this.makePurchase(
-        this.lexBotResponseObj.sessionAttributes.movieForPurchase
+    if (optionText === "schedule a demo") {
+      window.open(
+        "https://s3.amazonaws.com/alive5cdn/chat_window.html?wid=b0c58e09-4d41-4d7f-8595-18d05beee94e",
+        "_blank"
       );
+      this.showResponse(true, optionText);
+      this.sendTextMessageToBot(optionText);
     } else if (
       this.lexBotResponseObj.intentName === "PortlBuyAMovie" &&
       optionText !== "menu"
