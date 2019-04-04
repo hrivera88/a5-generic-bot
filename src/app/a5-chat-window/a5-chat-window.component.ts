a01aa75ea05acec3174de532ab828311498b578b
Halbert Rivera <hrivera@DUSTINs-MacBook-Pro.local>
2019-04-04 12:23:31 -0500

    Adding Bud Gardens Theme

--- src/app/a5-chat-window/a5-chat-window.component.ts
+++ src/app/a5-chat-window/a5-chat-window.component.ts
@@ -5,6 +5,7 @@ import {
   ViewChild,
   ElementRef
 } from "@angular/core";
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import {
   trigger,
   state,
@@ -80,6 +81,18 @@ export class A5ChatWindowComponent implements OnInit {
     email: "",
     serviceDetails: ""
   };
+  isTyping = false;
+  activeFAQDirectory = false;
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGl2ZV9wYXkiOnRydWUsImFsbG93X2J1bGtzbXMiOiIiLCJjaGFyZ2ViZWVfcGxhbiI6InByby1wbGFuIiwiY2hhcmdlYmVlX3BsYW5fbGFiZWwiOjksImNyZWRpdHNfdXNlZCI6OTQwOCwiaXNzIjoiYWxpdmU1X2FwaSIsIm1heF9jcmVkaXRzIjoyMzM1MCwib3JnX25hbWUiOiJhbGl2ZTVzdGFnZSIsInBheW1lbnRfbWV0aG9kIjoiY2hhcmdlYmVlIiwic2NyZWVuX25hbWUiOiJkdXN0aW4yIiwic3Vic2NyaXB0aW9uX2VuZF9kYXRlIjoxNTQ4NTgyNzMzLCJzdWJzY3JpcHRpb25faWQiOiJIcjU1MThuUW5VSVF0Yk5FMyIsInN1YnNjcmlwdGlvbl9zdGFydF9kYXRlIjoxNTQ1OTA0MzMzLCJ0eXBlIjoidXNlciIsInVzZXJfaWQiOiIzNzJmMWM2NS0xOWNhLTQwYzctOTJhOC01ZTJiMTNhMDU5MjMiLCJ1c2VyX3JvbGUiOiJhZG1pbiIsInZlcmlmaWVkIjp0cnVlLCJwb2xpY3lfaWQiOiJhMGY3MmMzMC1mYTdjLTQ5Y2EtODM1Mi1lNGZiZDYxMTJlMjMiLCJwb2xpY3kiOnsiY3JlYXRlZF9hdCI6MTU0MzMwNDE1NDY1MiwicG9saWN5X25hbWUiOiJhbGl2ZUNoYXQgRW5hYmxlZCIsInBvbGljeV9mZWF0dXJlcyI6WyJTTVMiLCJCT1RTIiwiYWxpdmVDaGF0IiwiQWxpdmVQYXkiLCJQSVBMIl0sInBvbGljeV9pZCI6ImEwZjcyYzMwLWZhN2MtNDljYS04MzUyLWU0ZmJkNjExMmUyMyJ9LCJpYXQiOjE1NDc2Njg3NDN9.5YDP1-SX0_6YH3GxKhPPNbeFjkb-2MMRtAM_HkwzpBQ",
+    }),
+    data: {
+      "org_name": "spectrabg",
+      "search": "",
+      "category_name": "Greetings",
+    }
+  };
   customEmojis = [
     {
       name: "Computer Guy",
@@ -150,12 +163,18 @@ export class A5ChatWindowComponent implements OnInit {
   ];
 
   // Customizing
-  windowBannerStyle = {
-    'background-color': '#fff',
-    'border-bottom-color': '#ff8359'
+  windowContainerStyle = {
+    'background': `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('/budweiser-gardens/assets/img/BudGardens-BW-Background.png') center center no-repeat`,
+    'border-left': `1px solid #ccc`,
+    'border-right': '1px solid #ccc',
+    'background-size': 'cover'
   };
-  logoImg = "/docjuris/assets/img/docjuris-logo.svg";
-  showGreetingSection = true;
+  windowBannerStyle = {
+    'background': 'rgba(0, 0, 0, 0.7)',
+    'border-bottom-color': '#f8f8f8'
+  };
+  logoImg = "/budweiser-gardens/assets/img/budweiser-logo.svg";
+  showGreetingSection = false;
   greetingLine = "Welcome to Doc Juris!";
   greetingSectionStyle = {
     'background-color': '#486290'
@@ -167,22 +186,30 @@ export class A5ChatWindowComponent implements OnInit {
   showBotOptions = false;
   botOptionImgSource = "../../assets/img/featurettes-header.png";
   botOptionsStyle = {
-    'background-color': '#486290'
+    'background': 'none'
   };
   botOptionButtonStyle = {
-    'border-color': '#fff',
-    color: '#fff'
+    'border-color': '#d71920',
+    color: '#fff',
+    'background': '#d71920'
   }
   botOptionTitleStyle = {
     'color': "#fff"
 
   }
+  messageListStyle = {
+    'background': 'none'
+  };
   sendButtonStyle = {
-    color: '#ff8359'
+    color: '#d71920'
+  };
+  messageSubmissionStyle = {
+    background: '#000'
   };
   constructor(
     private sendMailService: SendMailService,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private http: HttpClient
   ) {
     AWS.config.region = "us-east-1";
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
@@ -305,7 +332,111 @@ export class A5ChatWindowComponent implements OnInit {
       responseCards[this.currentResponseCardPosition].buttons
     );
   }
+  storeFAQAnswersLocalStorage(answers: any) {
+    localStorage.setItem('faq-answers', JSON.stringify(answers));
+  }
 
+  removeFAQAnswersLocalStorage() {
+    localStorage.removeItem('faq-answers');
+  }
+  sendAnswerToUser() {
+    let answers = localStorage.getItem('faq-answers');
+    let parsed = JSON.parse(answers);
+    if (parsed) {
+      if (parsed.length === 0) {
+        console.log('parsed equal zeero');
+        this.isTyping = false;
+        this.showResponse(false, 'Sorry, I couldn\'t help you out. Would you like to ask a human?');
+        this.removeFAQAnswersLocalStorage();
+        this.activeFAQDirectory = false;
+
+        setTimeout(() => {
+          this.botOptionsTitle = "Speak with a person?"
+          this.botMenuOptions = [
+            {
+              text: 'Text Us Your Question',
+              value: 'text us your question'
+            }];
+          this.showBotOptions = true;
+        }, 1000);
+      } else {
+        this.isTyping = false;
+        let answer = parsed.shift();
+        this.storeFAQAnswersLocalStorage(parsed);
+        this.showResponse(false, answer);
+        setTimeout(() => {
+          this.botOptionsTitle = 'Was this helpful?'
+          this.botMenuOptions = [
+            {
+              text: 'Yes',
+              value: 'yes'
+            },
+            {
+              text: 'No',
+              value: 'no'
+            }
+          ];
+          this.showBotOptions = true;
+        }, 1000);
+      }
+    }
+  }
+  sendSuccessFAQMessage() {
+    this.isTyping = false;
+    this.showResponse(false, 'Great, if you have any other questions let us know.');
+    this.removeFAQAnswersLocalStorage();
+    this.activeFAQDirectory = false;
+    setTimeout(() => {
+      this.showBotOptions = true;
+      this.botOptionsTitle = "View Main Menu?"
+      this.botMenuOptions = [
+        {
+          text: 'Main Menu',
+          value: 'menu'
+        }];
+    }, 1000);
+
+  }
+  makeCallToFAQAPI(userMessage: string) {
+    this.isTyping = true;
+    if (userMessage) {
+      this.httpOptions.data.search = userMessage;
+      this.http.get("https://api-v2.alive5.com/1.0/kb-article/search-external", {
+        headers: this.httpOptions.headers,
+        params: this.httpOptions.data
+      })
+        .subscribe((data: any) => {
+          if (data.error) {
+            console.log('subscride error');
+            this.isTyping = false;
+            this.showResponse(false, 'Sorry, I couldn\'t help you out. Would you like to ask a human?');
+            this.showBotOptions = true;
+            this.botMenuOptions = [
+              {
+                text: 'Text Us Your Question',
+                value: 'text us your question'
+              }];
+          } else {
+            let faqAnswersData = data.data;
+            this.storeFAQAnswersLocalStorage(faqAnswersData);
+            this.isTyping = false;
+            this.sendAnswerToUser();
+            this.activeFAQDirectory = true;
+          }
+          console.log(data);
+        });
+    } else {
+      this.isTyping = false;
+      console.log('no userMessage');
+      this.showResponse(false, 'Sorry, I couldn\'t help you out. Would you like to ask a human?');
+      this.showBotOptions = true;
+      this.botMenuOptions = [
+        {
+          text: 'Text Us Your Question',
+          value: 'text us your question'
+        }];
+    }
+  }
   showBotResponseToUser(botResponse) {
     //Display Bot's response to Chat UI
 
@@ -343,17 +474,35 @@ export class A5ChatWindowComponent implements OnInit {
   }
 
   submitMessageToBot(message: any) {
-    let usersMessage = this.botMessageInput.nativeElement.innerHTML;
-    usersMessage = usersMessage.replace(/<\s*div[^>]*>(.*?)<\s*\/\s*div>/g, "");
-    this.showResponse(true, usersMessage);
-    let noEmojisMsg = usersMessage.replace(/<\s*img[^>]*>/g, " ");
-    let noSpanTags = noEmojisMsg.replace(
-      /<\s*span[^>]*>(.*?)<\s*\/\s*span>/g,
-      ""
-    );
-    let cleanMessage = noSpanTags.replace(/&(nbsp|amp|quot|lt|gt);/g, " ");
-    this.sendTextMessageToBot(cleanMessage);
-    this.botMessageInput.nativeElement.innerHTML = "";
+    let messageUserTyped = this.botMessageInput.nativeElement.innerText;
+    messageUserTyped = messageUserTyped.replace(/(\r\n|\n|\r)/gm, "")
+    if (messageUserTyped === '') {
+      return;
+    }
+    this.showResponse(true, messageUserTyped);
+    this.botMessageInput.nativeElement.innerText = "";
+    this.multipleCards = false;
+    if (this.activeFAQDirectory === false) {
+      this.showBotOptions = false;
+      this.makeCallToFAQAPI(messageUserTyped);
+    } else {
+      // do stuff if theres a active local stored faq list
+      if (messageUserTyped.toLowerCase() === 'yes') {
+        this.isTyping = true;
+        this.showBotOptions = false;
+        this.sendSuccessFAQMessage();
+      } else if (messageUserTyped.toLowerCase() === 'no') {
+        this.sendAnswerToUser();
+        this.showBotOptions = false;
+      } else {
+        this.showResponse(false, 'Let me search for that real quick');
+        this.removeFAQAnswersLocalStorage();
+        this.showBotOptions = false;
+        setTimeout(() => {
+          this.makeCallToFAQAPI(messageUserTyped);
+        }, 1000);
+      }
+    }
   }
 
   sendTextMessageToBot(textMessage) {
@@ -361,7 +510,7 @@ export class A5ChatWindowComponent implements OnInit {
     // Gather needed parameters for Amazon Lex
     let params = {
       botAlias: "$LATEST",
-      botName: "DocJurisBot",
+      botName: "BudweiserGardens",
       inputText: textMessage,
       userId: this.lexUserID
     };
@@ -378,28 +527,91 @@ export class A5ChatWindowComponent implements OnInit {
     });
   }
 
+  triggerAliveChat() {
+    //for Hal's webbot
+    let alive5_sms_phone_number, alive5_sms_message_question;
+
+    if (window.location.pathname == '/budweiser-gardens') {
+      alive5_sms_phone_number = '+15196675700';
+      alive5_sms_message_question = 'I\'d like to connect with Budweiser Gardens Concierge [hit Send>]';
+    }
+
+    let alive5_pre_link;
+    let alive5_isDesktop = false;
+    let alive5_platform = navigator.platform.toUpperCase();
+    let alive5_isMobile = true;
+    let alive5_is_alive5_phone_number;
+
+    switch (alive5_platform) {
+      case 'IPAD':
+        alive5_isMobile = false;
+        //desktop Apple
+        if (alive5_is_alive5_phone_number == 'Y') {
+          alive5_pre_link = 'javascript:popAliveSMS(\'' + alive5_sms_phone_number + '\')';
+          alive5_isDesktop = true;
+        } else {
+          alive5_pre_link = 'javascript:void()';
+        }
+        break;
+      case 'IPHONE':
+        //see if 5, 6, or 7
+        //mobile
+        var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+          navigator.userAgent && !navigator.userAgent.match('CriOS');
+
+        if (isSafari && (window.self !== window.top)) {
+          //if safari AND in iframe. If in iframe, iMessages does not trigger, so open a new web page with a hack to run the SMS via redirect when page loads.
+          //mainly for WIX embed widget issue.
+          alive5_pre_link = "javascript:window.open( alive5_cdn_url + '/test/click.html?phone_number=" + alive5_sms_phone_number + "');";
+          alive5_isDesktop = true;
+        } else {
+          alive5_pre_link = 'sms:' + alive5_sms_phone_number + '&body=' + alive5_sms_message_question;
+        }
+        break;
+      default:
+        //ANDROID and Desktop others
+        //3/6/19 - chrome (72.0.3626.121) on android does not populate SMS
+        if (/Mobi/.test(navigator.userAgent)) {
+          //alive5_pre_link = 'sms:' + alive5_sms_phone_number + '?body=' + alive5_sms_message_question;
+          alive5_pre_link = 'sms://' + alive5_sms_phone_number + '?body=' + alive5_sms_message_question;
+        } else {
+          alive5_isMobile = false;
+          alive5_pre_link = 'javascript:popAliveSMS(\'' + alive5_sms_phone_number + '\')';
+          alive5_isDesktop = true;
+        }
+        break;
+    }
+
+    if (alive5_isDesktop) {
+      //currently desktop is not supported
+      //End alive5 Widget Code v2.0
+    } else {
+      //alive5_cta_button is your object/button you want enabled with SMS trigger
+      document.location.href = alive5_pre_link;
+    }
+  }
+
   chooseBotOption(evt: any) {
     let optionText = evt.target.value;
-    if (optionText === "schedule a demo") {
-      // window.open(
-      //   "https://s3.amazonaws.com/alive5cdn/chat_window.html?wid=b0c58e09-4d41-4d7f-8595-18d05beee94e",
-      //   "_blank"
-      // );
-      this.showResponse(true, optionText);
-      this.sendTextMessageToBot(optionText);
-    } else if (
-      this.lexBotResponseObj.intentName === "PortlBuyAMovie" &&
-      optionText !== "menu"
-    ) {
-      this.showAlivePayModal = true;
-      this.makePurchase(optionText);
-      this.showResponse(true, optionText);
-      this.sendTextMessageToBot(optionText);
-      this.bounceMenu = "button";
+    this.showResponse(true, optionText);
+    if (this.activeFAQDirectory === true) {
+      if (optionText === 'yes') {
+        this.isTyping = true;
+        this.sendSuccessFAQMessage();
+        this.showBotOptions = false;
+      } else {
+        this.isTyping = true;
+        this.sendAnswerToUser();
+        this.showBotOptions = false;
+      }
     } else {
-      this.showResponse(true, optionText);
-      this.sendTextMessageToBot(optionText);
-      this.bounceMenu = "button";
+      if (optionText === 'text us your question') {
+        this.triggerAliveChat();
+      } else {
+        this.sendTextMessageToBot(optionText);
+        this.bounceMenu = "button";
+        this.showBotOptions = false;
+      }
     }
   }
 
