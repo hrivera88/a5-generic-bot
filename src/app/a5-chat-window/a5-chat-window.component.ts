db900eb5d1b20c09964b438813e31de7d08bc753
Joshua Frasier <jafrasier50@hotmail.com>
2019-05-06 13:59:17 -0500

    adding thinker theme

--- src/app/a5-chat-window/a5-chat-window.component.ts
+++ src/app/a5-chat-window/a5-chat-window.component.ts
@@ -151,34 +151,33 @@ export class A5ChatWindowComponent implements OnInit {
 
   // Customizing ************
   windowBannerStyle = {
-    'background-color': '#fff',
-    'border-bottom-color': '#fad21c'
+    "background-color": "#59C7F0",
+    "border-bottom-color": "#0E9CB0"
   };
-  logoImg = "/assets/img/octobo-logo.png";
+  logoImg = "/assets/img/thinker-tinker_logo.png";
   showGreetingSection = true;
   greetingLine = "Welcome to Thinker-Tinker!";
   greetingSectionStyle = {
-    'background-color': '#045be4'
+    "background-color": "#75BA69"
   };
   greetingFontStyle = {
-    color: '#fff'
+    color: "#fff"
   };
   botOptionsImg = false;
   showBotOptions = false;
   botOptionImgSource = "../../assets/img/featurettes-header.png";
   botOptionsStyle = {
-    'background-color': '#045be4'
+    "background-color": "#59C7F0"
   };
   botOptionButtonStyle = {
-    'border-color': '#fff',
-    color: '#fff'
-  }
+    "border-color": "#fff",
+    color: "#fff"
+  };
   botOptionTitleStyle = {
-    'color': "#fff"
-
-  }
+    color: "#fff"
+  };
   sendButtonStyle = {
-    color: '#ff8359'
+    color: "#ff8359"
   };
   constructor(
     private sendMailService: SendMailService,
@@ -193,6 +192,9 @@ export class A5ChatWindowComponent implements OnInit {
 
   ngOnInit() {
     this.sendTextMessageToBot("menu");
+    setTimeout(() => {
+      this.sendTextMessageToBot("email");
+    }, 500);
     console.log(screen.width);
     if (screen.width < 768) {
       this.notMobileScreen = false;
@@ -361,7 +363,7 @@ export class A5ChatWindowComponent implements OnInit {
     // Gather needed parameters for Amazon Lex
     let params = {
       botAlias: "$LATEST",
-      botName: "DocJurisBot",
+      botName: "thinkerTinker",
       inputText: textMessage,
       userId: this.lexUserID
     };
@@ -382,9 +384,10 @@ export class A5ChatWindowComponent implements OnInit {
     //for Hal's webbot
     let alive5_sms_phone_number, alive5_sms_message_question;
 
-    if (window.location.pathname == '/budweiser-gardens') {
-      alive5_sms_phone_number = '+15196675700';
-      alive5_sms_message_question = 'I\'d like to connect with Budweiser Gardens Concierge [hit Send>]';
+    if (window.location.pathname == "/budweiser-gardens") {
+      alive5_sms_phone_number = "+15196675700";
+      alive5_sms_message_question =
+        "I'd like to connect with Budweiser Gardens Concierge [hit Send>]";
     }
 
     let alive5_pre_link;
@@ -394,29 +397,40 @@ export class A5ChatWindowComponent implements OnInit {
     let alive5_is_alive5_phone_number;
 
     switch (alive5_platform) {
-      case 'IPAD':
+      case "IPAD":
         alive5_isMobile = false;
         //desktop Apple
-        if (alive5_is_alive5_phone_number == 'Y') {
-          alive5_pre_link = 'javascript:popAliveSMS(\'' + alive5_sms_phone_number + '\')';
+        if (alive5_is_alive5_phone_number == "Y") {
+          alive5_pre_link =
+            "javascript:popAliveSMS('" + alive5_sms_phone_number + "')";
           alive5_isDesktop = true;
         } else {
-          alive5_pre_link = 'javascript:void()';
+          alive5_pre_link = "javascript:void()";
         }
         break;
-      case 'IPHONE':
+      case "IPHONE":
         //see if 5, 6, or 7
         //mobile
-        var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
-          navigator.userAgent && !navigator.userAgent.match('CriOS');
+        var isSafari =
+          navigator.vendor &&
+          navigator.vendor.indexOf("Apple") > -1 &&
+          navigator.userAgent &&
+          !navigator.userAgent.match("CriOS");
 
-        if (isSafari && (window.self !== window.top)) {
+        if (isSafari && window.self !== window.top) {
           //if safari AND in iframe. If in iframe, iMessages does not trigger, so open a new web page with a hack to run the SMS via redirect when page loads.
           //mainly for WIX embed widget issue.
-          alive5_pre_link = "javascript:window.open( alive5_cdn_url + '/test/click.html?phone_number=" + alive5_sms_phone_number + "');";
+          alive5_pre_link =
+            "javascript:window.open( alive5_cdn_url + '/test/click.html?phone_number=" +
+            alive5_sms_phone_number +
+            "');";
           alive5_isDesktop = true;
         } else {
-          alive5_pre_link = 'sms:' + alive5_sms_phone_number + '&body=' + alive5_sms_message_question;
+          alive5_pre_link =
+            "sms:" +
+            alive5_sms_phone_number +
+            "&body=" +
+            alive5_sms_message_question;
         }
         break;
       default:
@@ -424,10 +438,15 @@ export class A5ChatWindowComponent implements OnInit {
         //3/6/19 - chrome (72.0.3626.121) on android does not populate SMS
         if (/Mobi/.test(navigator.userAgent)) {
           //alive5_pre_link = 'sms:' + alive5_sms_phone_number + '?body=' + alive5_sms_message_question;
-          alive5_pre_link = 'sms://' + alive5_sms_phone_number + '?body=' + alive5_sms_message_question;
+          alive5_pre_link =
+            "sms://" +
+            alive5_sms_phone_number +
+            "?body=" +
+            alive5_sms_message_question;
         } else {
           alive5_isMobile = false;
-          alive5_pre_link = 'javascript:popAliveSMS(\'' + alive5_sms_phone_number + '\')';
+          alive5_pre_link =
+            "javascript:popAliveSMS('" + alive5_sms_phone_number + "')";
           alive5_isDesktop = true;
         }
         break;
