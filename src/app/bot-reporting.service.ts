99831c0fe2be63b5ef63d77dd6d91bf8e531cab1
Halbert Rivera <hrivera@DUSTINs-MacBook-Pro.local>
2019-05-21 14:55:45 -0500

    Created Bot Reporting Service

---
+++ src/app/bot-reporting.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class BotReportingService {
+  constructor(private http: HttpClient) {}
+
+  sendToReportingAPI(
+    action,
+    objectref,
+    groupid,
+    websiteid,
+    client_ip,
+    event_direction,
+    event_type,
+    event_content
+  ) {
+    return this.http.get("https://api-v1.websitealive.com/alive5", {
+      params: {
+        action: action,
+        objectref: objectref,
+        groupid: groupid,
+        websiteid: websiteid,
+        client_ip: client_ip,
+        event_direction: event_direction,
+        event_type: event_type,
+        event_content: event_content
+      }
+    });
+  }
+}
